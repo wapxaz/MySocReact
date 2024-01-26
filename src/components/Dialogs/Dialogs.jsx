0e8649a900917d386df56e1e70e5e56447c32df4
sukalovpro <poh@wapxaz.ru>
2024-01-26 15:07:43 +0800

    Узнал как рабоать с ref(считывание данных из textarea)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,6 +7,12 @@ const Dialogs = (props) => {
     let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id} img={d.img} />);
     let messageselements = props.state.messages.map(m => <Message message={m.message} id={m.id} />);
 
+    let newDialogElement = React.createRef();
+    let addPost = () => {
+        let text = newDialogElement.current.value;
+        alert(text);
+    };
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -14,6 +20,15 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messageselements}
+
+                <div>
+                    <div>
+                        <textarea ref={newDialogElement}></textarea>
+                    </div>
+                    <div>
+                        <button onClick={addPost}>Add post</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
