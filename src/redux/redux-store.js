e246f18b633398b483c2dae9db040c1d97210ee2
sukalovpro <poh@wapxaz.ru>
2024-02-05 23:56:22 +0800

    Добавил форму авторизации через redux-form

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,13 +5,15 @@ import sidebarReducer from "./sidebar-reducer.js";
 import usersReducer from "./users-reducer.js";
 import authReducer from "./auth-reducer.js";
 import {thunk as thunkMiddleware} from "redux-thunk";
+import { reducer as formReducer } from 'redux-form';
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogReducer,
     sidebarFriends: sidebarReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
