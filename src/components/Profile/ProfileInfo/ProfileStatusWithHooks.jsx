7b887dd1560ecfa432c2fa13dae0fca8d36646c5
sukalovpro <poh@wapxaz.ru>
2024-02-07 00:00:22 +0800

    хук useEffect(аналог componentDidMount и componentDidUpdate)

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,38 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ProfileStatusWithHooks = (props) => {
-    // state = {
-    //     editMode: false,
-    //     status: this.props.status
-    // }
-
-    // activateEditMode = () => {
-    //     this.setState({
-    //         editMode: true
-    //     });
-    // }
-    // deactivateEditMode = () => {
-    //     this.setState({
-    //         editMode: false
-    //     });
-    //     this.props.updateStatus(this.state.status);
-    // }
-    // onStatusChange = (e) => {
-    //     this.setState({
-    //         status: e.currentTarget.value
-    //     });
-    // }
-
-    // componentDidUpdate(prevProps, prevState) {
-    //     if (prevProps.status != this.props.status) {
-    //         this.setState({
-    //             status: this.props.status
-    //         });
-    //     }
-    // }
-
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
+
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]);
 
     const activateEditMode = () => {
         setEditMode(true);
