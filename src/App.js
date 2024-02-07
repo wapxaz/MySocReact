f3e8f90513f76bce9220bb6afb50ac5ea0068141
sukalovpro <poh@wapxaz.ru>
2024-02-07 20:27:59 +0800

    Ленивая подгрузка js страниц через Suspense и React.lazy

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import { Routes, Route } from 'react-router-dom';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import UsersContainer from './components/Users/UsersContainer';
-import ProfileContainer from './components/Profile/ProfilContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { initializeApp } from './redux/app-reducer';
 import { connect } from 'react-redux';
 import Preloader from './components/common/Preloader/Preloader';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/redux-store.js';
+
+//ленивая подгрузка js для страниц ниже
+//import DialogsContainer from './components/Dialogs/DialogsContainer';
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+//import ProfileContainer from './components/Profile/ProfileContainer';
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer.jsx'));
+//import UsersContainer from './components/Users/UsersContainer';
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer.jsx'));
 
 class App extends React.Component {
   componentDidMount() {
@@ -30,15 +35,17 @@ class App extends React.Component {
           <HeaderContainer />
           <Navbar />
           <div className='app-wrapper-content'>
-            <Routes>
-              <Route path='/dialogs/*' element={<DialogsContainer />} />
-              <Route path='/profile/:profileId?' element={<ProfileContainer />} />
-              <Route path='/users' element={<UsersContainer />} />
-              <Route path='/news' element={<News />} />
-              <Route path='/music' element={<Music />} />
-              <Route path='/settings' element={<Settings />} />
-              <Route path='/login' element={<Login />} />
-            </Routes>
+            <Suspense fallback={<div><Preloader /></div>}>
+              <Routes>
+                <Route path='/dialogs/*' element={<DialogsContainer />} />
+                <Route path='/profile/:profileId?' element={<ProfileContainer />} />
+                <Route path='/users' element={<UsersContainer />} />
+                <Route path='/news' element={<News />} />
+                <Route path='/music' element={<Music />} />
+                <Route path='/settings' element={<Settings />} />
+                <Route path='/login' element={<Login />} />
+              </Routes>
+            </Suspense>
           </div>
         </div>
       );
