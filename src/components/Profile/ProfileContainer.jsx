c396cda41f4d0bfe950740e2b4719572cf10b9b6
sukalovpro <poh@wapxaz.ru>
2024-02-07 23:33:38 +0800

    Перенес HOC withRouter в отдельный файл

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -2,26 +2,10 @@ import React from 'react';
 import Profile from './Profile';
 import { connect } from 'react-redux';
 import { getProfile, getStatus, updateStatus, savePhoto } from '../../redux/profile-reducer';
-import { Navigate, useLocation, useNavigate, useParams } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { withAuthRedirect } from '../../hoc/withAuthRedirect';
 import { compose } from 'redux';
-
-// wrapper to use react router's v6 hooks in class component(to use HOC pattern, like in router v5)
-function withRouter(Component) {
-  function ComponentWithRouterProp(props) {
-    let location = useLocation();
-    let navigate = useNavigate();
-    let params = useParams();
-    return (
-      <Component
-        {...props}
-        router={{ location, navigate, params }}
-      />
-    );
-  }
-
-  return ComponentWithRouterProp;
-}
+import { withRouter } from '../../hoc/withRouter';
 
 class ProfileContainer extends React.Component {
 
